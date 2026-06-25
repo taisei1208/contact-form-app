83ffd537858a5d2bc7a92a0a5d0ccda9f4f3ef4e
taisei <tsukuda12085735@gmail.com>
2026-06-26 04:47:53 +0900

    feat: モデル作成とリレーション定義 #2

---
+++ app/Models/Contact.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tel',
+        'address',
+        'building',
+        'detail',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function getGenderLabelAttribute(): string{
+        return match ($this->gender) {
+            1 => '男性',
+            2 => '女性',
+            default => 'その他',
+        };
+    }
+}
